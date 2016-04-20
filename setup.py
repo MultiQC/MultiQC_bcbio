f047917e814a1ae5cb9b4120683efd48aabb8f66
Lorena Pantano <lorena.pantano@gmail.com>
2016-04-20 15:32:36 -0400

    fix version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ For more information about MultiQC, see http://multiqc.info
 
 from setuptools import setup, find_packages
 
-version = '0.1.dev0'
+version = '0.1.0'
 
 setup(
     name = 'multiqc_bcbio',
