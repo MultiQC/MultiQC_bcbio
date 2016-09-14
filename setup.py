3d1b8bdca99f82a2a0d6934af72de22cafcfddb8
Lorena Pantano <lorena.pantano@gmail.com>
2016-09-14 19:28:31 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ For more information about MultiQC, see http://multiqc.info
 
 from setuptools import setup, find_packages
 
-version = '0.1.5'
+version = '0.1.6'
 
 setup(
     name = 'multiqc_bcbio',
