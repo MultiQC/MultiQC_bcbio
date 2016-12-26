d3be91cb1834408cf2754e79fd544db9b80a58c4
Lorena Pantano <lorena.pantano@gmail.com>
2016-12-26 12:00:53 +0100

    bump version to be compatible with multiqc 1.0dev

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ For more information about MultiQC, see http://multiqc.info
 
 from setuptools import setup, find_packages
 
-version = '0.1.9'
+version = '0.2.0dev'
 
 setup(
     name = 'multiqc_bcbio',
