ca76d8ae9d24fac4362def29e80847f0194fd697
vladsaveliev <vladsaveliev@me.com>
2017-05-17 19:09:14 +0300

    Using the `before_config` hook to update `config`

--- multiqc_bcbio/__init__.py
+++ multiqc_bcbio/__init__.py
@@ -1,47 +1,41 @@
 from __future__ import absolute_import
-from distutils.version import StrictVersion
 
 from .bcbio import MultiqcModule
-import multiqc
 from multiqc import config
-from multiqc.utils.config import update_dict
-from multiqc.utils.report import get_filelist
-
-update_dict(
-    config.sp, {'bcbio/metrics': {'fn': '*_bcbio.txt'},
-                'bcbio/coverage': {'fn': '*_bcbio_coverage.txt'},
-                'bcbio/coverage_avg': {'fn': '*_bcbio_coverage_avg.txt'},
-                'bcbio/variants': {'fn': '*_bcbio_variants.txt'},
-                'bcbio/target': {'fn': 'target_info.yaml'},
-                'bcbio/qsignature': {'fn': '*bcbio_qsignature.ma'},
-                'bcbio/vcfstats': {'fn': '*_bcbio_variants_stats.txt'},
-                'bcbio/seqbuster': {'contents': 'seqbuster'},
-                'bcbio/umi': {'fn': '*_umi_stats.yaml'},
-                'bcbio/viral': {'fn': '*viral*-counts.txt'},
-                'bcbio/damage': {'fn': '*damage.yaml'},
-                })
 
 
-config.fn_clean_exts.append({'type': 'regex', 'pattern': '_bcbio.*'})
-get_filelist()
+# Add search patterns and config options for the things that are used in MultiQC_bcbio
+def multiqc_bcbio_config():
+    """ Set up MultiQC config defaults for this package """
+    bcbio_search_patterns = {
+        'bcbio/metrics': {'fn': '*_bcbio.txt'},
+        'bcbio/coverage': {'fn': '*_bcbio_coverage.txt'},
+        'bcbio/coverage_avg': {'fn': '*_bcbio_coverage_avg.txt'},
+        'bcbio/variants': {'fn': '*_bcbio_variants.txt'},
+        'bcbio/target': {'fn': 'target_info.yaml'},
+        'bcbio/qsignature': {'fn': '*bcbio_qsignature.ma'},
+        'bcbio/vcfstats': {'fn': '*_bcbio_variants_stats.txt'},
+        'bcbio/seqbuster': {'contents': 'seqbuster'},
+        'bcbio/umi': {'fn': '*_umi_stats.yaml'},
+        'bcbio/viral': {'fn': '*viral*-counts.txt'},
+        'bcbio/damage': {'fn': '*damage.yaml'},
+    }
+    config.update_dict(config.sp, bcbio_search_patterns)
 
-
-for module, value_dict in {
-    'FastQC': {
-        'percent_duplicates': False,
-        'total_sequences': False,
-    },
-    'QualiMap': {
-        'percentage_aligned': False,
-    },
-    'Samtools Stats': {
-        'non-primary_alignments': False,
-        'reads_mapped': False,
-        'reads_mapped_percent': False,
-        'raw_total_sequences': False,
-    }}.items():
-
-    if module not in config.table_columns_visible:
-        config.table_columns_visible[module] = dict()
-    config.table_columns_visible[module].update(value_dict)
-
+    config.fn_clean_exts.append({'type': 'regex', 'pattern': '_bcbio.*'})
+    
+    config.update_dict(config.table_columns_visible, {
+        'FastQC': {
+            'percent_duplicates': False,
+            'total_sequences': False,
+        },
+        'QualiMap': {
+            'percentage_aligned': False,
+        },
+        'Samtools Stats': {
+            'non-primary_alignments': False,
+            'reads_mapped': False,
+            'reads_mapped_percent': False,
+            'raw_total_sequences': False,
+        }
+    })
