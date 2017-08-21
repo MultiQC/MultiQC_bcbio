6d532abd95adaf3c07761528718b3cbf42efc3b9
chapmanb <chapmanb@50mail.com>
2017-08-21 10:44:53 -0400

    v0.2.1: MultiQC 1.2 and settings fixes

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ For more information about MultiQC, see http://multiqc.info
 
 from setuptools import setup, find_packages
 
-version = '0.2.0'
+version = '0.2.1'
 
 setup(
     name = 'multiqc_bcbio',
