8bb8f019419f7527c19a131fa56a0bbf1818babe
roryk <rory.kirchner@gmail.com>
2020-10-29 11:08:36 -0400

    Prep for 0.2.9 release.

---
+++ HISTORY.md
@@ -0,0 +1,5 @@
+## 0.3.0 (in progress)
+
+## 0.2.9
+- Cleaner viral metrics.
+- Add missing damage filter data.
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ For more information about MultiQC, see http://multiqc.info
 
 from setuptools import setup, find_packages
 
-version = '0.2.8'
+version = '0.2.9'
 
 setup(
     name = 'multiqc_bcbio',
